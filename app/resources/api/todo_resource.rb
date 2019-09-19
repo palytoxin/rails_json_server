7d8d6e06582a6b0f030e265aac3beb9c3ded4589
yan <sxty32@gmail.com>
2019-09-19 14:00:47 +0800

    add user_id when fetch todo

--- app/resources/api/todo_resource.rb
+++ app/resources/api/todo_resource.rb
@@ -1,6 +1,14 @@
 class Api::TodoResource < JSONAPI::Resource
-  attributes :title, :description, :created_at
+  attributes :title, :description, :created_at, :user_id
   has_one :user
 
   filter :title
+
+  # def self.updatable_fields(context)
+  #   super - [:user_id]
+  # end
+
+  # def self.creatable_fields(context)
+  #   super - [:user_id]
+  # end
 end
